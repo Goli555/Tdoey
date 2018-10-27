e73c6126a83afe882f709d234ced6bfe23c499b7
Hidekazu Sato <goli@hidekazunomacbook-pro.local>
2018-10-27 18:28:50 +0900

    Data persisted using UserDefaults method.

--- Tdoey/TodoListViewController.swift
+++ Tdoey/TodoListViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["アイウエオ","abcdef","位fdsじゃいふぁsだ"]
+    var itemArray = ["アイウエオ","abcdef","位fdsじゃいふぁsだ"]
+    
+    let defaults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        if let items = defaults.array(forKey: "TodoListArray")as? [String]  {
+            itemArray = items
+        }
         
     }
     
@@ -47,10 +52,41 @@ class TodoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
-        
 
     }
-
+    
+    //MARK - Add New Item
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+//        アラートの型は、中央部に出るか（alert）か、下部に出るか？（actionsheet）アラートの出し方は、まずは箱を作って、ボタンをくっつけて表示する命令を追加する
+        let alert = UIAlertController(title: "新しいItemの追加", message: "リストに新しいItemを追加します。", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happpen once the clicks the Add Item button onoer UIAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "追加するItemの文章を入れてください。"
+            textField = alertTextField
+            
+            print(alertTextField.text)
+            print("今だよ")
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil )
+        
+    }
+    
 
 }
 
